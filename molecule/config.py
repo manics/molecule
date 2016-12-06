1921246eeebf1e0abb4fa655b058634eaa50122e
John Dewey <john@dewey.ws>
2016-12-06 14:02:01 -0800

    Removed check mode from running in test cycle (#632)
    Check mode quite frankly is a pain in the ass.  Having it run by default
    without the ability to turn it off only causes pain.  We have written
    a lot of real-world roles, and we end up disabling check mode most of
    the time.
    
    This PR disables check mode by default, but the user is free to add it
    to their test sequence if they want it.

--- molecule/config.py
+++ molecule/config.py
@@ -175,7 +175,7 @@ class ConfigV1(Config):
                 'test': {
                     'sequence': [
                         'destroy', 'dependency', 'syntax', 'create',
-                        'converge', 'idempotence', 'check', 'verify'
+                        'converge', 'idempotence', 'verify'
                     ]
                 },
                 'testinfra_dir': 'tests',
